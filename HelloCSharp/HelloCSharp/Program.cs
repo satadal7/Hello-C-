12c443584e557084e4ba2608fc79bfcac12fb000
Литвинюк Виталий <stawr.93@gmail.com>
2016-03-03 16:23:57 +0600

    Hello!

---
+++ HelloCSharp/HelloCSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloCSharp
+{
+  using System;
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello, C#!");
+      Console.ReadKey();
+    }
+  }
+}
+
+
